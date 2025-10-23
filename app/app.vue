e6d70fae840d777ed31243832199800f5b84072f
amillanbc <amillan@bluecorela.com>
2025-10-23 14:01:43 -0500

    Ft: General header comp

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,12 @@
 <template>
   <NuxtRouteAnnouncer />
+  <GeneralHeader v-if="route.fullPath !== '/'" />
   <NuxtPage />
   <GeneralFooter />
 </template>
 
 <script setup>
+const route = useRoute()
 onMounted(() => {
   document.documentElement.classList.remove('dark')
 })
---
+++ app/components/general/GeneralHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="fixed top-0 left-0 z-10 w-full bg-black text-white">
+    <UContainer>
+      <div class="mx-auto flex max-w-7xl items-center justify-between py-2">
+        <img :src="OSStudioLogoWhite" class="h-auto w-[165px]" />
+        <div class="hidden gap-8 sm:flex">
+          <span
+            v-for="(link, i) in navLinks"
+            :key="i"
+            @click="goToPath(link.path)"
+            :class="`cursor-pointer text-lg ${gradientText(link.path)}`"
+          >
+            {{ link.label }}
+          </span>
+        </div>
+      </div>
+    </UContainer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import OSStudioLogoWhite from '@/assets/images/os-studio-logo-white.svg'
+
+const route = useRoute()
+const router = useRouter()
+const { toggleDrawer, navLinks } = NavDrawerComp()
+
+const gradientText = (path: string) => (route.fullPath === path ? 'studio-gradient-text' : '')
+
+const goToPath = (path: string) => router.push(path)
+</script>
